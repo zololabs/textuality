73a96443fd9af573d650ef4d2c66237e7d3c86d1
Sheroy Marker <sheroy@gmail.com>
2013-08-11 19:52:03 -0700

    Removes DOM parsing logic from core

--- src/zolo/textuality/core.clj
+++ src/zolo/textuality/core.clj
@@ -1,18 +1,2 @@
-(ns zolo.textuality.core
-  (:require [clojure.zip :as zip]
-            [clojure.xml :as xml]
-            [clojure.walk :as walk]))
-
-(import 'org.jsoup.Jsoup)
-
-(defn text-from
-  ([html]
-     (.text (.body (Jsoup/parse html)))))
-
-(defn zip-str [s]
-  (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
-
-(defn convert-html
-  [html]
-  (walk/postwalk #(do (println "visiting: " %) %) (zip-str html)))
+(ns zolo.textuality.core)
 
